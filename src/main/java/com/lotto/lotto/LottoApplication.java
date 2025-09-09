0bbdc2eb3f7565a2bc287781434e923b77597916
yehor1959 <zaitsev.yehor@gmail.com>
2025-09-09 18:50:27 +0200

    Add RandomNumberGeneratorRestTemplateConfigurationProperties

--- src/main/java/com/lotto/lotto/LottoApplication.java
+++ src/main/java/com/lotto/lotto/LottoApplication.java
@@ -1,16 +1,13 @@
 package com.lotto.lotto;
 
 import com.lotto.lotto.domain.numbergenerator.WinningNumbersGeneratorFacadeConfigurationProperties;
-import com.lotto.lotto.infrastructure.AsdadadRepository;
-import com.lotto.lotto.infrastructure.Ticket;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+import com.lotto.lotto.infrastructure.numbergenerator.http.RandomNumberGeneratorRestTemplateConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({WinningNumbersGeneratorFacadeConfigurationProperties.class})
+@EnableConfigurationProperties({WinningNumbersGeneratorFacadeConfigurationProperties.class, RandomNumberGeneratorRestTemplateConfigurationProperties.class})
 public class LottoApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/lotto/lotto/infrastructure/numbergenerator/http/RandomNumberGeneratorRestTemplateConfigurationProperties.java
@@ -0,0 +1,12 @@
+package com.lotto.lotto.infrastructure.numbergenerator.http;
+
+import lombok.Builder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "lotto.number-generator.http.client.config")
+@Builder
+public record RandomNumberGeneratorRestTemplateConfigurationProperties(String uri,
+                                                                       int port,
+                                                                       int connectionTimeout,
+                                                                       int readTimeout) {
+}
